e48dcd1905d95f588be4fa1b409388196d5f2968
qinmingyuan <mingyuan0715@foxmail.com>
2018-11-06 14:45:37 +0800

    remove unused assets fonts files

--- app/assets/config/rails_log_manifest.js
+++ app/assets/config/rails_log_manifest.js
@@ -1,3 +1,1 @@
-//= link rails_log/application.css
-//= link rails_log/application.js
 //= link_tree ../javascripts/controllers .js
--- lib/tasks/rails_log_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :rails_log do
-#   # Task goes here
-# end
