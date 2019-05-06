cfb365d7d3ebde63d47373afef0f55ba9a0c6e08
qinmingyuan <mingyuan0715@foxmail.com>
2019-05-06 19:43:30 +0800

    log

--- lib/rails_log/controller_subscriber.rb
+++ lib/rails_log/controller_subscriber.rb
@@ -6,10 +6,12 @@ module RailsLog
       payload = event.payload
       raw_headers = payload.fetch(:headers, {})
       real_headers = ::LogRecord.request_headers(raw_headers)
+      session_key = Rails.configuration.session_options[:key]
+      cookies = raw_headers['rack.request.cookie_hash'].except(:session_key)
 
       debug "  Headers: #{real_headers.inspect}"
       debug "  Sessions: #{raw_headers['rack.session'].to_h}"
-      debug "  Cookies: #{raw_headers['rack.request.cookie_hash']}"
+      debug "  Cookies: #{cookies}"
     end
 
     def process_action(event)
