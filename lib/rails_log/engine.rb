e8eb7cf4e9a903a595a5f730982a34bdc0c05a40
qinmingyuan <mingyuan0715@foxmail.com>
2019-08-24 21:12:44 +0800

    js with webpacker/import

--- lib/rails_log/engine.rb
+++ lib/rails_log/engine.rb
@@ -20,12 +20,7 @@ module RailsLog
     end
 
     initializer 'rails_log.assets.precompile' do |app|
-      app.config.assets.precompile += ['rails_log_manifest.js']
       ApplicationMailer.include MailerRecord
-    end
-
-    initializer 'rails_log.add_assets_templates' do |app|
-      app.config.assets.paths.push *Dir[File.expand_path('lib/nondigest_assets/*', root)]
     end
 
     initializer 'rails_log.quiet_logs' do |app|
