b10d0e1d0eaf0b63b2af85b36ec6b1807fb320bf
qinmingyuan <mingyuan0715@foxmail.com>
2020-10-29 10:42:35 +0800

    add defaults to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  scope :panel, module: 'logged/panel', as: :panel do
+  scope :panel, module: 'logged/panel', as: :panel, defaults: { namespace: 'panel', business: 'logged' } do
     resources :log_records, only: [:index, :show, :destroy]
     resources :log_mailers, only: [:index, :show, :destroy]
     resources :log_csps, only: [:index, :show, :destroy]
   end
 
-  scope module: 'logged' do
+  scope module: 'logged', defaults: { business: 'logged' } do
     controller :rails_log do
       get '/not_founds' => :index
       post '/csp_violation_report' => :csp
@@ -14,8 +14,7 @@ Rails.application.routes.draw do
   end
 
 end
-if RailsLog.config.intercept_not_found
-  Rails.application.routes.append do
-    match '*path' => 'logged/rails_log#not_found', via: :all
-  end
-end
+
+Rails.application.routes.append do
+  match '*path' => 'logged/rails_log#not_found', via: :all
+end if RailsLog.config.intercept_not_found
