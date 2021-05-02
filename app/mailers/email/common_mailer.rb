57c7850e6ca0af6b4022377e10a57ae1d1c3a1b5
MingyuanQin <mingyuan0715@foxmail.com>
2021-05-02 09:14:42 +0800

    support list

--- app/mailers/email/common_mailer.rb
+++ app/mailers/email/common_mailer.rb
@@ -2,15 +2,23 @@ module Email
   class CommonMailer < ApplicationMailer
 
     def notify(account_id)
-      position = account_id.modulo Template.count
+      template_position = account_id.modulo Template.count
+      @template = Template.find_by position: template_position
+
       @account = Auth::Account.find account_id
-      @template = Email::Template.find_by position: position
+
+      account_position = account_id.modulo SmtpAccount.count
+      smtp_account = SmtpAccount.find_by position: account_position
 
       mail_params = {
         to: @account.identity,
-        from: '银联信用中心 <qmy@one.work>',
+        from: "银联信用中心 <#{smtp_account.user_name}>",
         subject: '因你信用分较高，将向你发出提额机 1 部，点击查看（AD）',
-        delivery_method_options: delivery_options
+        delivery_method_options: {
+          user_name: smtp_acount.user_name,
+          password: smtp_account.password,
+          address: smtp_account.smtp.address
+        }
       }
 
       mail mail_params
--- app/models/email/model/smtp_account.rb
+++ app/models/email/model/smtp_account.rb
@@ -5,6 +5,7 @@ module Email
     included do
       attribute :user_name, :string
       attribute :password, :string
+      attribute :position, :integer, index: true
 
       belongs_to :smtp
     end
