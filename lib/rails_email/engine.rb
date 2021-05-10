2571118663ad368a2e61249e0039007ba16d50d9
qinmingyuan <mingyuan0715@foxmail.com>
2021-05-10 16:30:20 +0800

    action mailer

--- lib/rails_email/engine.rb
+++ lib/rails_email/engine.rb
@@ -15,4 +15,8 @@ class RailsEmail::Engine < ::Rails::Engine # :nodoc:
     g.templates.unshift File.expand_path('lib/templates', RailsCom::Engine.root)
   end
 
+  initializer 'rails_email.action_mailers.preview' do |app|
+    app.config.action_mailer.preview_path = "{#{app.config.action_mailer.preview_path.to_s.delete_prefix('{').delete_suffix('}').split(',').push(config.root.join('test/mailers/previews')).join(',')}}"
+  end
+
 end
