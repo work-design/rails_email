b2291f73989d8f50bd83f148815b5691c4edef8d
qinmingyuan <mingyuan0715@foxmail.com>
2021-05-18 18:17:48 +0800

    email

--- app/models/email/account.rb
+++ app/models/email/account.rb
@@ -1,8 +1,7 @@
 module Email
   class Account < ApplicationRecord
     include Model::Account
-
-    self.table_name = 'accounts'
+    self.table_name = 'auth_accounts'
     self.inheritance_column = nil
   end
 end
