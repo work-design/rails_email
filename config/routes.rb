f0192757a9c7a92a3a3547d52d4c4293d9eca4b5
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-24 20:53:08 +0800

    gemilfe

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,24 @@
 Rails.application.routes.draw do
-
-  namespace :email, defaults: { business: 'email' } do
-    controller :home do
-      get :index
-      get :preview
-    end
-    resources :subscriptions
-    namespace :panel, defaults: { namespace: 'panel' } do
-      resources :logs, only: [:index, :show, :destroy]
-      resources :smtps
-      resources :reasons
-    end
-    namespace :admin, defaults: { namespace: 'admin' } do
-      resources :smtps do
-        resources :smtp_accounts
+  scope RailsCom.default_routes_scope do
+    namespace :email, defaults: { business: 'email' } do
+      controller :home do
+        get :index
+        get :preview
       end
-      resources :logs
       resources :subscriptions
-      resources :templates
+      namespace :panel, defaults: { namespace: 'panel' } do
+        resources :logs, only: [:index, :show, :destroy]
+        resources :smtps
+        resources :reasons
+      end
+      namespace :admin, defaults: { namespace: 'admin' } do
+        resources :smtps do
+          resources :smtp_accounts
+        end
+        resources :logs
+        resources :subscriptions
+        resources :templates
+      end
     end
   end
-
 end
