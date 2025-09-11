a8650e77809d60f1a17ee900f84deed8d1988a09
qinmingyuan <mingyuan0715@foxmail.com>
2025-09-11 20:48:41 +0800

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,23 @@
 Rails.application.routes.draw do
-  scope RailsCom.default_routes_scope do
-    namespace :email, defaults: { business: 'email' } do
-      controller :home do
-        get :index
-        get :preview
+  namespace :email, defaults: { business: 'email' } do
+    controller :home do
+      get :index
+      get :preview
+    end
+    resources :subscriptions
+    namespace :panel, defaults: { namespace: 'panel' } do
+      root 'home#index'
+      resources :logs, only: [:index, :show, :destroy]
+      resources :smtps
+      resources :reasons
+    end
+    namespace :admin, defaults: { namespace: 'admin' } do
+      resources :smtps do
+        resources :smtp_accounts
       end
+      resources :logs
       resources :subscriptions
-      namespace :panel, defaults: { namespace: 'panel' } do
-        root 'home#index'
-        resources :logs, only: [:index, :show, :destroy]
-        resources :smtps
-        resources :reasons
-      end
-      namespace :admin, defaults: { namespace: 'admin' } do
-        resources :smtps do
-          resources :smtp_accounts
-        end
-        resources :logs
-        resources :subscriptions
-        resources :templates
-      end
+      resources :templates
     end
   end
 end
